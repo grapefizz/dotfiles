cc6bdf5e5d40b801085ef67bcf43c67be85d71c4
grapefizz <arikarakushi@proton.me>
2025-08-07 22:40:15 +0200

    added sketchybar config because I forgot

---
+++ config/sketchybar/items/power.sh
@@ -0,0 +1,16 @@
+sketchybar --add item battery right \
+           --set battery update_freq=3 \
+                         icon.drawing=on \
+                         script="/Users/Ari/.config/sketchybar/plugins/power.sh" \
+                         background.color=0x003C3E4F \
+                         background.padding_left=-5
+
+sketchybar --add item power_logo right \
+           --set power_logo icon= \
+                 icon.color=0xffB3E1A7 \
+                 label.drawing=on \
+                 background.color=0x00FFFFFF
+
+
+
+
--- modules/home.nix
+++ modules/home.nix
@@ -33,7 +33,7 @@
       }
     ];
     initContent = ''
-      # Catppuccin Mocha theme for fzf (with transparency)
+      # Catppuccin Mocha theme for fzf
       export FZF_DEFAULT_OPTS=" \
       --color=bg+:#313244,spinner:#F5E0DC,hl:#F38BA8 \
       --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
@@ -135,6 +135,26 @@
   home.file.".config/fastfetch".source = ../config/fastfetch;
   home.file.".config/sketchybar".source = ../config/sketchybar;
   home.file.".config/wallpapers".source = ../config/wallpapers;
+
+  # Activation script to set wallpaper using desktoppr
+  home.activation.setWallpaper = config.lib.dag.entryAfter ["writeBoundary"] ''
+    wallpaper_path="${config.home.homeDirectory}/.config/wallpapers/venice-market.png"
+    desktoppr_path="/usr/local/bin/desktoppr"
+    
+    if [[ -f "$wallpaper_path" ]]; then
+      if [[ -x "$desktoppr_path" ]]; then
+        echo "Setting wallpaper with desktoppr: $wallpaper_path"
+        "$desktoppr_path" "$wallpaper_path"
+        echo "Wallpaper set successfully!"
+      else
+        echo "Warning: desktoppr not found at $desktoppr_path. Make sure it's installed via Homebrew."
+      fi
+    else
+      echo "Warning: Wallpaper file not found at $wallpaper_path (you prob have the wrong file extension ya dummy)"
+      echo "Available wallpapers:"
+      ls "${config.home.homeDirectory}/.config/wallpapers/" || echo "Wallpapers directory not found"
+    fi
+  '';
 
 }
 
